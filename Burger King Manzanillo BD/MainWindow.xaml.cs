875f6e69ff1db0186c2cf9dabf49b261428502c1
Antonio Juarez <jjuarez18@ucol.mx>
2019-10-18 11:36:08 -0500

    Se termino corectamente

--- Burger King Manzanillo BD/MainWindow.xaml.cs
+++ Burger King Manzanillo BD/MainWindow.xaml.cs
@@ -80,12 +80,12 @@ namespace Burger_King_Manzanillo_BD
 
             if (txtNumPed.Text != "")
             {
-                if (txtPanWhp.IsEnabled == true)
+                if (txtNumPed.IsEnabled == true)
                 {
                     if (txtPanJr.Text != "Ingresa la cantidad de panes")
                     {
-                        cmd.CommandText = "insert into Pedidos_Bimbo(Numero_de_pedido,Pan_Whopper,Pan_Whopper_Jr,Fecha_de_pedido) " +
-                            "Values(" + txtNumPed.Text + "','" + txtPanWhp.Text + "','" + txtPanJr.Text + ",'" + txtPanMedias.Text + ",'" + txtFecha.Text + "')";
+                        cmd.CommandText = "insert into Pedidos_Bimbo(Numero_de_pedido,Pan_Whopper,Pan_Whopper_Jr,Pan_Medias_Lunas,Fecha_de_pedido) " +
+                            "Values(" + txtNumPed.Text + ",'" + txtPanWhp.Text + "','" + txtPanJr.Text + "','" + txtPanMedias.Text + "','" + txtFecha.Text + "')";
                         cmd.ExecuteNonQuery();
                         MostrarDatos();
                         MessageBox.Show("El pedido se registró correctamente...");
@@ -99,7 +99,7 @@ namespace Burger_King_Manzanillo_BD
                 }
                 else
                 {
-                    cmd.CommandText = "update Programming Students set Pan_Whopper='" + txtPanWhp.Text + "',Pan_Whopper_Jr='" + txtPanJr.Text + "',Pan_Medias_Lunas=" + txtPanMedias.Text
+                    cmd.CommandText = "update Pedidos_Bimbo set Pan_Whopper='" + txtPanWhp.Text + "',Pan_Whopper_Jr='" + txtPanJr.Text + "',Pan_Medias_Lunas=" + txtPanMedias.Text
                         + ",Fecha_de_pedido='" + txtFecha.Text + "' where Numero_de_Pedido=" + txtNumPed.Text;
                     cmd.ExecuteNonQuery();
                     MostrarDatos();
@@ -116,22 +116,50 @@ namespace Burger_King_Manzanillo_BD
         
         private void BtnEditar_Click_1(object sender, RoutedEventArgs e)
         {
+            if (gvDatos.SelectedItems.Count > 0)
+            {
+                DataRowView row = (DataRowView)gvDatos.SelectedItems[0];
+                txtNumPed.Text = row["Numero_de_Pedido"].ToString();
+                txtPanWhp.Text = row["Pan_Whopper"].ToString();
+                txtPanJr.Text = row["Pan_Whopper_Jr"].ToString();
+                txtPanMedias.Text = row["Pan_Medias_Lunas"].ToString();
+                txtFecha.Text = row["Fecha_de_pedido"].ToString();
+                txtNumPed.IsEnabled = false;
+                btnNuevo.Content = "Actualizar";
+            }
+            else
+            {
+                MessageBox.Show("Favor de seleccionar el pedido...");
+            }
 
         }
 
         private void BtnBorrar_Click_1(object sender, RoutedEventArgs e)
         {
-
+            if (gvDatos.SelectedItems.Count > 0)
+            {
+                DataRowView row = (DataRowView)gvDatos.SelectedItems[0];
+                OleDbCommand cmd = new OleDbCommand();
+                if (con.State != ConnectionState.Open)
+                    con.Open();
+                cmd.Connection = con;
+                cmd.CommandText = "Delete from Pedidos_Bimbo where Numero_de_Pedido=" +
+               row["Numero_de_Pedido"].ToString();
+                cmd.ExecuteNonQuery();
+                MostrarDatos();
+                MessageBox.Show("Pedido eliminado correctamente...");
+                LimpiaTodo();
+            }
         }
 
         private void BtnCancelar_Click_1(object sender, RoutedEventArgs e)
         {
-
+            LimpiaTodo();
         }
 
         private void BtnSalir_Click_1(object sender, RoutedEventArgs e)
         {
-
+            Application.Current.Shutdown();
         }
 
 
